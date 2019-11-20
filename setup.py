1abf5935c20c0a297a3f36ada1e5a37d171e345f
Hatatister <hendrik.levering@yahoo.de>
2019-11-21 00:06:23 +0100

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="smart-injector",
-    version='0.0.2',
+    version='0.0.3',
     license="BSD-2-Clause",
     description="An easy to use lightweight dependency injection framework",
     long_description="%s\n%s"
