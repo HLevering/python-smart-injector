00891dcbc135067f515046c37ba6c345ee061ed9
Hatatister <hendrik.levering@yahoo.com>
2020-05-27 21:49:05 +0200

    added docstrings

--- src/smart_injector/utility.py
+++ src/smart_injector/utility.py
@@ -8,6 +8,7 @@ T = TypeVar("T")
 
 
 def get_return_type(a_type: Callable[..., T]) -> Optional[Type[T]]:
+    """returns the return type of a callable if it is available"""
     r_type = inspect.signature(a_type).return_annotation
     if r_type is inspect.Signature.empty:
         return None
